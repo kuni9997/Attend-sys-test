e67763e9c366a559bc060d89e8dc9d4827b47c8e
Kuniyoshi Mitstuta <mitsuta.kuni999@gmail.com>
2023-05-14 02:06:02 +0900

    first

---
+++ src/resources/views/layouts/header.blade.php
@@ -0,0 +1,14 @@
+    <header class="header">
+        <div class='header-logo'>
+            <h1 class="header-logo--title">Atte</h1>
+            @auth
+            <nav>
+                <ul class="header-nav">
+                    <li><a>ホーム</a> </li>
+                    <li><a>日付一覧</a></li>
+                    <li><a>ログアウト</a></li>
+                </ul>
+            </nav>
+            @endauth
+        </div>
+    </header>
